a0ca35f77c55710e92e48615069c49410a889abd
Priyono Santosa <psantosa@leverate.co.id>
2023-03-23 11:10:54 +0700

    add setup 2

--- pages/index.vue
+++ pages/index.vue
@@ -105,23 +105,3 @@ export default {
 };
 </script>
 
-
-// <script setup>
-// useHead({
-//   title: "Alun - Music App for Your Well-Being Acivities",
-//   // or, instead:
-//   // titleTemplate: (title) => `My App - ${title}`,
-//   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-//   charset: "utf-8",
-//   meta: [
-//     {
-//       name: "description",
-//       content:
-//         "AI generated ambient sound that's personalized to improve your mental health daily activities.",
-//     },
-//   ],
-//   bodyAttrs: {
-//     class: "alun",
-//   },
-// });
-// </script>
