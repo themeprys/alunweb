c95ea0fdb9f1d77430e50e665d3fd2460f68574b
Priyono Santosa <psantosa@leverate.co.id>
2023-03-23 08:55:01 +0700

    add thank you

--- components/ThankyouBeta.vue
+++ components/ThankyouBeta.vue
@@ -11,11 +11,9 @@
         />
         <h1 class="display-5 fw-bold">Sign Up Success</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">
-            Thank You for joining our Beta Testing Program. Once our app is
-            ready, you will be the first person to get early access of Alun App.
-          </p>
-          <p>STAY TUNED!</p>
+            <p class="lead mb-4">Thank you for registering your email and agreeing to be<br>a beta tester for Alun.</p>
+            <p>After this, we will frequently send newsletters and the latest information about Alun's development to your email. When the Alun beta version is ready, you will be the first among the others to try using the Alun beta version.</p>
+            <!-- <p>STAY TUNED!</p> -->
           <!-- <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
               Primary button
