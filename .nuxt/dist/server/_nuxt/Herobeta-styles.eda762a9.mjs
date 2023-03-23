0636c2f19bcecfcdef0c1e9f9d385ba279dc7e2c
Priyono Santosa <psantosa@leverate.co.id>
2023-03-23 12:14:21 +0700

    add gtm

--- .nuxt/dist/server/_nuxt/Herobeta-styles.eda762a9.mjs
+++
@@ -1,2 +0,0 @@
-import style_0 from './Herobeta-styles-1.mjs.7e2964ff.js';
-export default [style_0]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,7 @@
 export default defineNuxtConfig({
     app: {
         head: {
-            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
-            
-        },
-        
+            link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],  
+        },        
     },
-    modules: ['nuxt-gtag'],
-    gtag: {
-      id: 'G-2B33HH8Y84'
-    }    
 })
